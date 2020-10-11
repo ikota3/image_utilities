d917ab06c4a09cc3b6ab37e647f8452b4fbf6971
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-10-11 21:57:44 +0900

    Update images_to_pdf.py and readme

--- README.md
+++ README.md
@@ -2,9 +2,37 @@
 
 Convert images to pdf with CLI.
 
-## Convert images to pdf
+## Usage
+
+### Convert images to pdf
+
+#### Parameters
+
+- `-i`, `--input_dir`
+  - **required parameter.**
+  - input directory.
+- `-o`, `--output_dir`
+  - **required parameter.**
+  - output directory.
+- `-e`, `--extensions`
+  - **optional parameter.**
+  - **default paramter is `jpg`**
+  - image extension type.
+- `-f`, `--force_write`
+  - **optional parameter.**
+  - **default paramter is `False`**
+  - force to write pdf file.
+
+#### Note
+
+- This command will recursively watch the input directory, and convert the **images in each directory** to pdf.
+
+- `-e`, `--extensions` can pass multiple extension. But you have to pass a list, with no space between comma.
+
+  - e.g. `-e jpg,png`
+
+#### Example
 
 ```sh
-$ python src/images_to_pdf.py convert -i "path/to/images" -o "path/to/output"
-TODO
+python src/images_to_pdf.py convert -i "path/to/input" -o "path/to/output" -e jpg,png -f
 ```
--- src/images_to_pdf.py
+++ src/images_to_pdf.py
@@ -1,17 +1,67 @@
 import os
 import fire
+import img2pdf
 from PIL import Image
 
 
 class PDFConverter(object):
     """Convert images to pdf class."""
 
-    def __init__(self, input_dir=None, output_dir=None):
-        self.input_dir = input_dir
-        self.output_dir = output_dir
+    def __init__(
+            self,
+            input_dir: str = "",
+            output_dir: str = "",
+            extensions: (str) = None,
+            force_write: bool = False
+    ):
+        self.input_dir: str = input_dir
+        self.output_dir: str = output_dir
+        if not extensions:
+            extensions = ['.jpg']
+        self.extensions: (str) = tuple(extensions)
+        self.force_write: bool = bool(force_write)
+
+    def _input_is_valid(self) -> bool:
+        is_valid = True
+        if not os.path.isdir(self.input_dir):
+            is_valid = False
+            print('[ERROR] You must type a valid directory for input directory.')
+
+        if not os.path.isdir(self.output_dir):
+            is_valid = False
+            print('[ERROR] You must type a valid directory for output directory.')
+
+        return is_valid
 
     def convert(self):
-        pass
+        if not self._input_is_valid():
+            return
+
+        for current_dir, dirs, files in os.walk(self.input_dir):
+            images = []
+            for filename in sorted(files):
+                if filename.endswith(self.extensions):
+                    path = os.path.join(current_dir, filename)
+                    images.append(path)
+
+            pdf_filename = os.path.join(
+                self.output_dir, f'{os.path.basename(current_dir)}.pdf'
+            )
+
+            if self.force_write:
+                if images:
+                    with open(pdf_filename, 'wb') as f:
+                        f.write(img2pdf.convert(images))
+                    print(f'[INFO] Created {pdf_filename}!')
+            else:
+                if os.path.exists(pdf_filename):
+                    print(f"[ERROR] {pdf_filename} already exist!")
+                    continue
+
+                if images:
+                    with open(pdf_filename, 'wb') as f:
+                        f.write(img2pdf.convert(images))
+                    print(f'[INFO] Created {pdf_filename}!')
 
 
 if __name__ == '__main__':
