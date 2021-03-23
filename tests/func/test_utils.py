c66c7dfeba8b4a4e65039d19a13aac31e36fb6c0
ikota3 <51883336+ikota3@users.noreply.github.com>
2021-03-23 23:22:52 +0900

    Refactor test code

--- tests/func/test_utils.py
+++ tests/func/test_utils.py
@@ -5,24 +5,24 @@ def test_append_prefix__string_should_return_tuple_of_one_item_prefix_added():
     targets = 'jpg'
     prefix = '.'
     ret_val = append_prefix(targets, prefix)
-    expected = ('.jpg',)
-    assert ret_val == expected
-    assert isinstance(expected, tuple)
+    expected_val = ('.jpg',)
+    assert ret_val == expected_val
+    assert isinstance(expected_val, tuple)
 
 
 def test_append_prefix__list_should_return_tuple_of_multiple_item_prefix_added():
     targets = ['jpg', 'jpeg', 'png']
     prefix = '.'
     ret_val = append_prefix(targets, prefix)
-    expected = ('.jpg', '.jpeg', '.png',)
-    assert ret_val == expected
-    assert isinstance(expected, tuple)
+    expected_val = ('.jpg', '.jpeg', '.png',)
+    assert ret_val == expected_val
+    assert isinstance(expected_val, tuple)
 
 
 def test_append_prefix__tuple_should_return_tuple_of_multiple_item_prefix_added():
     targets = ('jpg', 'jpeg', 'png',)
     prefix = '.'
     ret_val = append_prefix(targets, prefix)
-    expected = ('.jpg', '.jpeg', '.png',)
-    assert ret_val == expected
-    assert isinstance(expected, tuple)
+    expected_val = ('.jpg', '.jpeg', '.png',)
+    assert ret_val == expected_val
+    assert isinstance(expected_val, tuple)
